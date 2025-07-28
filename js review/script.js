d2ce66a7c3c97b05edda199152d893b1da2fcc87
Dezmon <dezcoleman2@gmail.com>
2025-07-27 22:34:56 -0500

    first quiz commit

---
+++ js review/script.js
@@ -0,0 +1,12 @@
+const questions = [
+  {
+    question:
+      "What is the keyword to declare a variable. Choose all that apply.",
+    answers: [
+      { text: "var", correct: true },
+      { text: "let", correct: true },
+      { text: "int", correct: false },
+      { text: "const", correct: true },
+    ]
+  }
+];
