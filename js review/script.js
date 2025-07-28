09e60cff346e5fde10348181b9f00577d117d60d
Dezmon <dezcoleman2@gmail.com>
2025-07-28 06:25:28 -0500

    quiz question commit

--- js review/script.js
+++ js review/script.js
@@ -8,5 +8,47 @@ const questions = [
       { text: "int", correct: false },
       { text: "const", correct: true },
     ]
+  },
+  {
+    question: "What does DOM stand for?",
+    answers: [
+      { text: "Document Object Model", correct: true },
+      { text: "Data Output Mode", correct: false },
+      { text: "Digital Ordinance Method", correct: false },
+      { text: "Display Object Mapping", correct: false }
+    ]
   }
 ];
+
+const questionContainer = document.getElementById("question-container");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+
+function showQuestion(question) {
+  questionContainer.innerHTML = question.question;
+  answerButtons.innerHTML = "";
+  question.answers.forEach(answer => {
+    const btn = document.createElement("button");
+    btn.innerText = answer.text;
+    btn.classList.add("btn");
+    if (answer.correct) {
+      btn.dataset.correct = answer.correct;
+    }
+    btn.addEventListener("click", selectAnswer);
+    answerButtons.appendChild(btn);
+  });
+}
+
+function selectAnswer(e) {
+  // For now, just log which button was clicked
+  console.log("Answer selected:", e.target.innerText);
+}
+
+
+
+// nextButton.addEventListener("click", handleNextButton);
+
+showQuestion(questions[currentQuestionIndex]);
+nextButton.style.display = "none";
